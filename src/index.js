31ce2499dd16690e4b96f13ffe523ef807f3278e
danielle lane <daniellelane1821@gmail.com>
2019-09-11 14:51:41 -0500

    added a require  and exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 console.log("hello, world");
+
+const niceMessage = require('./say-hello');
+console.log(niceMessage);
---
+++ src/say-hello.js
@@ -0,0 +1,4 @@
+const niceMessage = () => `howdy, i like to do da chaha like a sissy girl`;
+
+
+module.exports = niceMessage();
